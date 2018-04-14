b78755ffede2336b8a5d155b3f1dddec8c084c0e
Carl Anderson <carl.anderson@vadose.org>
2018-04-14 17:04:13 -0400

    add script to check downloads

---
+++ file_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# call this as
+#  find data/2017 -exec bash ./file_check.sh {} \;
+
+fil="$1"
+
+if [ -f $fil ]; then
+    lastLine=`tail -n 1 $fil`
+    if [ ! "${lastLine:0:9}" == "</Return>" ]; then
+        echo "# ${lastLine:0:9}"
+        echo "rm $fil"
+    fi
+fi
